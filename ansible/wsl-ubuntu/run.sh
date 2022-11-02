32eb1e82d664f58dbee51048578d9401b53056c2
Lenny <lenny.donnez@gmail.com>
2022-11-02 23:37:31 +0100

    Install ansible through pip
    Makes sure latest version is available

--- ansible/wsl-ubuntu/run.sh
+++ ansible/wsl-ubuntu/run.sh
@@ -11,11 +11,11 @@ sudo apt update && sudo apt upgrade
 
 sudo apt install git python python-setuptools python3-setuptools python3-pip
 
-# install ansible
-sudo apt install ansible
-
 # upgrade pip
 pip3 install --user --upgrade pip
+
+# install ansible
+pip3 install ansible
 
 # run playbook
 ansible-playbook setup_wsl_ubuntu.yml --ask-become-pass
