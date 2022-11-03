3f13347e83ed34a4c972c6c6545d91a467dcafb6
Lenny <lenny.donnez@gmail.com>
2022-11-03 03:30:25 +0100

    Add configuration specific for wsl
    Makes sure no PATH variables of Windows will be added to the wsl env

--- ansible/wsl-ubuntu/run.sh
+++ ansible/wsl-ubuntu/run.sh
@@ -17,5 +17,10 @@ pip3 install --user --upgrade pip
 # install ansible
 pip3 install ansible
 
-# run playbook
-ansible-playbook setup_wsl_ubuntu.yml --ask-become-pass
+# install wsl config
+sudo cp files/wsl.conf /etc/
+
+echo "************************************************************************************************************"
+echo "Run ansible-playbook setup_wsl_ubuntu.yml -K in this dir after running wsl --shutdown in a Powershell shell"
+echo "************************************************************************************************************"
+sleep 5s
