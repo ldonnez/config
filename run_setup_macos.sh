b0b45c6f7f4cedbc61969dbe954c925cdbbbd3fb
Lenny <lenny.donnez@gmail.com>
2023-10-05 09:48:39 +0200

    Make brew command available in shell

--- run_setup_macos.sh
+++ run_setup_macos.sh
@@ -5,6 +5,11 @@ cd "$(dirname "$0")"
 # install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# Make brew command available in shell
+if [[ $(uname -m) == 'arm64' ]] && [[ $(uname) = "Darwin" ]]; then
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
 # update brew
 brew update && brew upgrade
 
