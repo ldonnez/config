f72b91eefeb0484b86376e393f9cfd8b09220fd8
Lenny <lenny.donnez@gmail.com>
2024-07-07 20:10:31 +0200

    Add entry point setup script for Debian

---
+++ run_setup_debian.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+
+cd "$(dirname "$0")"
+
+# Create .local/bin to symlink alternative paths locally
+mkdir -p ~/.local/bin
+
+# update apt
+sudo apt update && sudo apt upgrade
+
+sudo apt install git python3-setuptools python3-pip pipx python3-apt ansible
+
+# install ansible-lint
+pipx install ansible-lint
+
+# Install requirements
+ansible-galaxy install -r requirements.yml
+
+# Run playbook
+ansible-playbook setup_debian.yml -K --tags install
